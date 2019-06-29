05686a06a8f3b4eabf856f0f5e838e67dba0e60c
ClaudiaAriasH <36428554+ClaudiaAriasH@users.noreply.github.com>
2019-06-29 14:57:37 -0500

    Initial commit

---
+++ src/components/EarthImage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img
+      src="https://epic.gsfc.nasa.gov/archive/natural/2015/10/31/png/epic_1b_20151031074844.png"
+      alt
+    >
+
+    <div>
+      <h3>Tierra</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EarthImage"
+};
+</script>
+
+<style scoped>
+</style>
